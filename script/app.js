cd38416739ec9b8f0193b04ef1e81e53bc5e9134
jauumfm <61278539+jauumfm@users.noreply.github.com>
2024-08-26 22:11:23 -0300

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,40 @@
+
+
+
+var Decodificar = document.querySelector('#textoDecodificar');
+Decodificar.addEventListener("input", function(e){
+  e.target.value = e.target.value.toLowerCase()
+  ;
+});//transforma tudo em minusculo//
+
+function removeAccents(str) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); //remove acento//
+  }
+
+
+var Decodificado = document.querySelector("#textoDecodificado");
+
+function Criptografando(){
+
+    var semAcento = Decodificar.value;
+    var letra = removeAccents(semAcento);
+    //var letraDecodificada = letra.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat"); fazendo dessa maneira gera bug teste com ai
+    var letraDecodificada = letra.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+    document.getElementById('textoDecodificado').innerHTML = '<textarea readonly id="textoConfigurado">' + letraDecodificada + '</textarea>' + '<button class= "copiar" id="botaoCopia" onclick="copiando()">Copiar</button>'
+}
+
+
+function Desriptografando(){
+    var semAcento = Decodificar.value;
+    var letra = removeAccents(semAcento);
+
+    var letraDescodificada = letra.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    document.getElementById('textoDecodificado').innerHTML = '<textarea readonly id="textoConfigurado">' + letraDescodificada + '</textarea>' + '<button class= "copiar" id="botaoCopia" onclick="copiando()"> Copiar </button>'
+}
+
+function copiando(){
+    var textoCopiado = document.getElementById('textoConfigurado').innerHTML;
+
+    navigator.clipboard.writeText(textoCopiado);
+}
